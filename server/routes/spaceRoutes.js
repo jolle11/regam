1e48b3958e7e96611a80b130c8a38a1d8baff031
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-10-20 16:56:38 +0200

    Created space routes

--- server/routes/spaceRoutes.js
+++ server/routes/spaceRoutes.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect } = require("../middleware/authMiddleware");
+
 const {
 	getSpaces,
+	getSpace,
+	getDays,
 	setSpace,
-	updateSpace,
+	setDay,
+	updateDay,
 	deleteSpace,
 } = require("../controllers/spaceController");
+
+// Show spaces
+router.get("/", getSpaces);
+// Show specific space, delete specific space
+router.route("/:id").get(protect, getSpace).delete(protect, deleteSpace);
+// Show space days
+router.get("/:id/days", getDays);
+// Create new space
+router.post("/create", setSpace);
+// Create new day
+router.post("/:id/days/create", setDay);
+// Update day
+router.patch("/:id/days/update", updateDay);
 
 module.exports = router;
