dd9b8dff6edbfab03f6ba155154cab5a9be457bf
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-11-29 18:31:49 +0100

    Added apiSlice and middleware

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import { apiSlice } from "../features/spaces/spaceApiSlice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		[apiSlice.reducerPath]: apiSlice.reducer,
+	},
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware().concat(apiSlice.middleware);
 	},
 });
 
