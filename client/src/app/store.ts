cda2152b821040f8775a3ebb804d4958f50ebb6c
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-01 19:47:30 +0100

    Added spaceArray reducer

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import spaceReducer from "../features/spaces/spaceSlice";
+import spaceArrayReducer from "../features/spaces/spaceArraySlice";
 import dayReducer from "../features/days/daysSlice";
 import { spacesApiSlice } from "../features/spaces/spacesApiSlice";
 import { daysApiSlice } from "../features/days/daysApiSlice";
@@ -9,6 +10,7 @@ export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		space: spaceReducer,
+		spaceArray: spaceArrayReducer,
 		day: dayReducer,
 		[spacesApiSlice.reducerPath]: spacesApiSlice.reducer,
 		[daysApiSlice.reducerPath]: daysApiSlice.reducer,
