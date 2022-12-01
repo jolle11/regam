3d6da8e0f59e00b553fdd79b35841da8adc1f194
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-01 19:47:57 +0100

    Started filling the component

--- client/src/pages/Space/Space.tsx
+++ client/src/pages/Space/Space.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
 import "./Space.scss";
 
 const Space = () => {
-	return <div>Space</div>;
+	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
+
+	const space = useAppSelector((state) => state.space);
+
+	return (
+		<>
+			<h1>{space.name}</h1>
+		</>
+	);
 };
 
 export default Space;
