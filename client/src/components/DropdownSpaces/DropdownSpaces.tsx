14ffafa9d9af3979d4b2461890d56115e708b503
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-01 19:45:44 +0100

    Added spaces to dropdown

--- client/src/components/DropdownSpaces/DropdownSpaces.tsx
+++ client/src/components/DropdownSpaces/DropdownSpaces.tsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+
+import { setSpace } from "../../features/spaces/spaceSlice";
+
+import { Day, Space } from "../../ts";
 
 const DropdownSpaces = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
+
+	const spaceArray = useAppSelector((state) => state.spaceArray);
 
 	const handleDropdown = () => {
 		setIsOpen(!isOpen);
+	};
+
+	const handleSpaceSwitch = (space: Space) => {
+		dispatch(setSpace(space));
+		navigate(`/space/${space.name}`);
 	};
 
 	return (
@@ -15,8 +30,11 @@ const DropdownSpaces = () => {
 				<>
 					<button>New Space</button>
 					<ul>
-						{/* TODO: Map de spaces */}
-						{/* <Link to={`/spaces/${space.name}`}></Link> */}
+						{spaceArray.map((space: Space) => (
+							<li key={space.id} onClick={() => handleSpaceSwitch(space)}>
+								{space.name}
+							</li>
+						))}
 					</ul>
 				</>
 			)}
