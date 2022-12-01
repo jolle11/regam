3d5b9c388b36850c1236782e85d7a9619b1b346d
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-01 19:49:08 +0100

    Added route to specific space

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { Home, Login, Register } from "./pages";
+import { Home, Space, Login, Register } from "./pages";
 import { Header } from "./components";
 
 function App() {
@@ -13,10 +13,7 @@ function App() {
 					<Header />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						{/* TODO: Spaces will come from the dropdown */}
-						{/* <Route path="/spaces" element={<Spaces />} /> */}
-						{/* TODO: Space will be a page */}
-						{/* <Route path="/space/:spaceName" element={<Space />} /> */}
+						<Route path="/space/:name" element={<Space />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/register" element={<Register />} />
 					</Routes>
