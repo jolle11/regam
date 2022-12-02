c089dcb4ec37afe69fa93797676a8b88abf278c0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-02 20:23:48 +0100

    Added SingleDay component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,7 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Spinner from "./Spinner/Spinner";
 import DropdownSpaces from "./DropdownSpaces/DropdownSpaces";
+import SingleDay from "./SingleDay/SingleDay";
 import DaysCounter from "./DaysCounter/DaysCounter";
 import SpaceCard from "./SpaceCard/SpaceCard";
 
-export { Header, Footer, Spinner, DropdownSpaces, DaysCounter, SpaceCard };
+export { Header, Footer, Spinner, DropdownSpaces, SingleDay, DaysCounter, SpaceCard };
