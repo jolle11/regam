a35e20383cf8d5eeb6ff5474cf2a1a8a9291c020
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-02 20:25:55 +0100

    Filled up

--- client/src/pages/Space/Space.tsx
+++ client/src/pages/Space/Space.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+
+import { DaysCounter, SingleDay } from "../../components";
+
+import { useFetchDaysQuery } from "../../features/days/daysApiSlice";
 
 import "./Space.scss";
 
@@ -11,9 +15,38 @@ const Space = () => {
 
 	const space = useAppSelector((state) => state.space);
 
+	const { data = [], isFetching, refetch } = useFetchDaysQuery(space);
+
 	return (
 		<>
 			<h1>{space.name}</h1>
+			<DaysCounter />
+			<table>
+				<tbody>
+					<tr>
+						<th>Dia</th>
+						<th>R</th>
+						<th>F</th>
+						<th>T</th>
+						<th>Accions</th>
+						<th>Comentaris</th>
+					</tr>
+				</tbody>
+			</table>
+			{console.log(data)}
+			{!isFetching &&
+				data.map((day) => (
+					<SingleDay
+						key={day.id}
+						id={day.id}
+						space={day.space}
+						date={day.date}
+						water={day.water}
+						fertilizer={day.fertilizer}
+						transplant={day.transplant}
+						comment={day.comment}
+					/>
+				))}
 		</>
 	);
 };
