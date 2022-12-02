ddc9e1152494f4bdf91a2778f4584d94fa7046cb
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-02 20:21:03 +0100

    Filled up

--- client/src/components/DaysCounter/DaysCounter.tsx
+++ client/src/components/DaysCounter/DaysCounter.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import "./DaysCounter.scss";
 
 const DaysCounter = () => {
-	return <div>DaysCounter</div>;
+	return (
+		<div className="counter">
+			<div className="counter__title">Dies des de l'útim:</div>
+			<div className="counter__subtitle counter__subtitle--rec">Rec</div>
+			<div className="counter__subtitle counter__subtitle--fertilitzant">Fertilitzant</div>
+			<div className="counter__subtitle counter__subtitle--transplant">Transplant</div>
+			<div className="counter__number counter__number--rec">3</div>
+			<div className="counter__number counter__number--fertilitzant">28</div>
+			<div className="counter__number counter__number--transplant">105</div>
+		</div>
+	);
 };
 
 export default DaysCounter;
