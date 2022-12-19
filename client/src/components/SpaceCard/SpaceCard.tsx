d083db277421ffebb7ca123c34186e887e484aae
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-19 11:15:51 +0100

    Added functionality

--- client/src/components/SpaceCard/SpaceCard.tsx
+++ client/src/components/SpaceCard/SpaceCard.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-
+import { useAppDispatch } from "../../app/hooks";
+import { setSpace } from "../../features/spaces/spaceSlice";
+import { Space } from "../../ts";
 import "./SpaceCard.scss";
 
-const SpaceCard = () => {
-	return <div>SpaceCard</div>;
+const SpaceCard = (space: Space) => {
+	const dispatch = useAppDispatch();
+
+	return (
+		<div key={space.id}>
+			<h3>{space.name}</h3>
+			<button onClick={() => dispatch(setSpace(space))}>Set as current space</button>
+		</div>
+	);
 };
 
 export default SpaceCard;
