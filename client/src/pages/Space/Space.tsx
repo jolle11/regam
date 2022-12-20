4586bb8689dc72c68e3c6f43fc1233566df3fc55
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-20 17:38:29 +0100

    Set up way to pass last date of each needed for the days counter

--- client/src/pages/Space/Space.tsx
+++ client/src/pages/Space/Space.tsx
@@ -2,19 +2,52 @@ import { useAppSelector } from "../../app/hooks";
 import { CreateDay, DaysCounter, SingleDay } from "../../components";
 import { useFetchDaysQuery } from "../../features/days/daysApiSlice";
 import "./Space.scss";
+import { useEffect, useState } from "react";
 
 const Space = () => {
 	const space = useAppSelector((state) => state.space);
 
+	const [lastWater, setLastWater] = useState<string[]>([]);
+	const [lastFertilizer, setLastFertilizer] = useState<string[]>([]);
+	const [lastTransplant, setLastTransplant] = useState<string[]>([]);
+
 	const { data = [], isFetching } = useFetchDaysQuery(space);
+
+	data.map((day) => {
+		if (day.water === true) {
+			lastWater.push(day.date);
+		} else if (day.fertilizer === true) {
+			lastFertilizer.push(day.date);
+		} else if (day.transplant === true) {
+			lastTransplant.push(day.date);
+		}
+	});
+
+	useEffect(() => {
+		setLastWater([]);
+		setLastFertilizer([]);
+		setLastTransplant([]);
+	}, [isFetching]);
 
 	return (
 		<>
 			<div>
 				<h1>{space.name}</h1>
 			</div>
-			{/*TODO: Figure out how to count the days inside the counter*/}
-			{/* <DaysCounter /> */}
+			{/* {data.map((day) => {
+				if (day.water === true) {
+					lastWater.push(day.date);
+				} else if (day.fertilizer === true) {
+					lastFertilizer.push(day.date);
+				} else if (day.transplant === true) {
+					lastTransplant.push(day.date);
+				}
+			})} */}
+			<DaysCounter
+				water={lastWater.slice(-1).toString()}
+				fertilizer={lastFertilizer.slice(-1).toString()}
+				transplant={lastTransplant.slice(-1).toString()}
+			/>
 			<CreateDay />
 			<div className="space__days">
 				{!isFetching && data.map((day) => <SingleDay key={day.id} {...day} />)}
