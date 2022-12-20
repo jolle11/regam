fb609e7eb94a43b2c6173c89f159e3a0cd608363
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-20 17:36:40 +0100

    Added props and formatted accordingly

--- client/src/components/DaysCounter/DaysCounter.tsx
+++ client/src/components/DaysCounter/DaysCounter.tsx
@@ -1,15 +1,27 @@
 import "./DaysCounter.scss";
+import moment from "moment";
+interface IProps {
+	water: string;
+	fertilizer: string;
+	transplant: string;
+}
 
-const DaysCounter = () => {
+const DaysCounter = ({ water, fertilizer, transplant }: IProps) => {
 	return (
 		<div className="counter">
 			<div className="counter__title">Dies des de l'útim:</div>
 			<div className="counter__subtitle counter__subtitle--rec">Rec</div>
 			<div className="counter__subtitle counter__subtitle--fertilitzant">Fertilitzant</div>
 			<div className="counter__subtitle counter__subtitle--transplant">Transplant</div>
-			<div className="counter__number counter__number--rec">3</div>
-			<div className="counter__number counter__number--fertilitzant">28</div>
-			<div className="counter__number counter__number--transplant">105</div>
+			<div className="counter__number counter__number--rec">
+				{moment(water, "DD/MM/YYYY").fromNow()}
+			</div>
+			<div className="counter__number counter__number--fertilitzant">
+				{moment(fertilizer, "DD/MM/YYYY").fromNow()}
+			</div>
+			<div className="counter__number counter__number--transplant">
+				{moment(transplant, "DD/MM/YYYY").fromNow()}
+			</div>
 		</div>
 	);
 };
