e85eb3cadc1cb4ad811db365fb84e9dcad2718d8
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-20 13:00:29 +0100

    Set up space update and deletion

--- client/src/components/SpaceCard/SpaceCard.tsx
+++ client/src/components/SpaceCard/SpaceCard.tsx
@@ -1,15 +1,71 @@
 import { useAppDispatch } from "../../app/hooks";
 import { setSpace } from "../../features/spaces/spaceSlice";
+import {
+	useDeleteSpaceMutation,
+	useUpdateSpaceMutation,
+} from "../../features/spaces/spacesApiSlice";
 import { Space } from "../../ts";
 import "./SpaceCard.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SpaceCard = (space: Space) => {
+	const navigate = useNavigate();
 	const dispatch = useAppDispatch();
 
+	const [handleUpdate] = useUpdateSpaceMutation();
+	const [handleDelete] = useDeleteSpaceMutation();
+
+	const [toggleSpaceActions, setToggleSpaceActions] = useState<boolean>(false);
+	const [isEdited, setIsEdited] = useState<boolean>(false);
+	const [editableName, setEditableName] = useState<string>("");
+
 	return (
-		<div key={space.id}>
+		<div>
 			<h3>{space.name}</h3>
-			<button onClick={() => dispatch(setSpace(space))}>Set as current space</button>
+			<button
+				onClick={() => {
+					dispatch(setSpace(space));
+					navigate(`/space/${space.name}`);
+				}}
+			>
+				Seleccionar espai
+			</button>
+			<button
+				onClick={() => {
+					setToggleSpaceActions(!toggleSpaceActions);
+					setEditableName("");
+				}}
+			>
+				{toggleSpaceActions ? "Cancellar" : "Editar espai"}
+			</button>
+			{toggleSpaceActions && (
+				<>
+					<input
+						onChange={(e: React.FormEvent<HTMLInputElement>) => {
+							setEditableName(e.currentTarget.value || "");
+							setIsEdited(false);
+						}}
+					/>
+					<button
+						disabled={isEdited}
+						onClick={() => {
+							handleUpdate({ id: space.id, name: editableName });
+							setIsEdited(true);
+						}}
+					>
+						Actualitzar espai
+					</button>
+					<button
+						// TODO: Add two step deletion
+						onClick={() => {
+							handleDelete(space.id);
+						}}
+					>
+						Eliminar espai
+					</button>
+				</>
+			)}
 		</div>
 	);
 };
