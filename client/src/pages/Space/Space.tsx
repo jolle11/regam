b923c86c9bd8f936370b994560c62d78411c9a59
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-20 13:02:14 +0100

    Deleted unused code

--- client/src/pages/Space/Space.tsx
+++ client/src/pages/Space/Space.tsx
@@ -1,17 +1,10 @@
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { useAppSelector } from "../../app/hooks";
 import { CreateDay, DaysCounter, SingleDay } from "../../components";
 import { useFetchDaysQuery } from "../../features/days/daysApiSlice";
-import { useUpdateSpaceMutation } from "../../features/spaces/spacesApiSlice";
 import "./Space.scss";
-import { useState } from "react";
 
 const Space = () => {
 	const space = useAppSelector((state) => state.space);
-	const dispatch = useAppDispatch();
-	const [handleUpdate] = useUpdateSpaceMutation();
-
-	const [isEdited, setIsEdited] = useState<boolean>(false);
-	const [editableName, setEditableName] = useState<string>(space.name);
 
 	const { data = [], isFetching } = useFetchDaysQuery(space);
 
@@ -19,23 +12,6 @@ const Space = () => {
 		<>
 			<div>
 				<h1>{space.name}</h1>
-				{/*TODO: Figure out where is the best place to use update space (???component, dropdown, space page???)*/}
-				{/* <input
-					defaultValue={space.name}
-					onChange={(e: React.FormEvent<HTMLInputElement>) => {
-						setEditableName(e.currentTarget.value || "");
-						setIsEdited(false);
-					}}
-				/>
-				<button
-					disabled={editableName !== space.name && isEdited}
-					onClick={() => {
-						handleUpdate({ id: space.id, name: editableName });
-						setIsEdited(true);
-					}}
-				>
-					Actualitzar espai
-				</button> */}
 			</div>
 			{/*TODO: Figure out how to count the days inside the counter*/}
 			{/* <DaysCounter /> */}
