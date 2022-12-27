1cfb0a160c70e77927913045917d7593e72ededb
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-27 12:10:31 +0100

    Refactor and use of new function for the days

--- client/src/components/DaysCounter/DaysCounter.tsx
+++ client/src/components/DaysCounter/DaysCounter.tsx
@@ -1,5 +1,7 @@
+import getLanguageCount from "../../utils/getLanguageCount";
 import "./DaysCounter.scss";
 import moment from "moment";
+
 interface IProps {
 	water: string;
 	fertilizer: string;
@@ -7,20 +9,22 @@ interface IProps {
 }
 
 const DaysCounter = ({ water, fertilizer, transplant }: IProps) => {
+	const lastWater = moment(water, "DD/MM/YYYY").fromNow();
+	const lastFertilizer = moment(fertilizer, "DD/MM/YYYY").fromNow();
+	const lastTransplant = moment(transplant, "DD/MM/YYYY").fromNow();
+
 	return (
 		<div className="counter">
-			<div className="counter__title">Dies des de l'útim:</div>
+			<div className="counter__title">Útim:</div>
 			<div className="counter__subtitle counter__subtitle--rec">Rec</div>
 			<div className="counter__subtitle counter__subtitle--fertilitzant">Fertilitzant</div>
 			<div className="counter__subtitle counter__subtitle--transplant">Transplant</div>
-			<div className="counter__number counter__number--rec">
-				{moment(water, "DD/MM/YYYY").fromNow()}
-			</div>
+			<div className="counter__number counter__number--rec">{getLanguageCount(lastWater)}</div>
 			<div className="counter__number counter__number--fertilitzant">
-				{moment(fertilizer, "DD/MM/YYYY").fromNow()}
+				{getLanguageCount(lastFertilizer)}
 			</div>
 			<div className="counter__number counter__number--transplant">
-				{moment(transplant, "DD/MM/YYYY").fromNow()}
+				{getLanguageCount(lastTransplant)}
 			</div>
 		</div>
 	);
