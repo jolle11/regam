8e4b9a7f47c3adab3ec58a970d828f2e31d1158e
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-27 12:08:46 +0100

    Created function to translate to catalan the days past from last date in water, fertilizer and transplant

---
+++ client/src/utils/getLanguageCount.ts
@@ -0,0 +1,25 @@
+const getPositionElement = (string: string, position: number) => {
+	return string.split(" ").at(position);
+};
+
+const getLanguageCount = (date: string) => {
+	switch (getPositionElement(date, 1)) {
+		default:
+			if (date === "Invalid date") {
+				return "Hi ha hagut algún problema";
+			}
+			return `Fa ${getPositionElement(date, 0)} dies`;
+		case "hours":
+			return `Fa ${getPositionElement(date, 0)} hores`;
+		case "month":
+			return `Fa ${getPositionElement(date, 0)} mes`;
+		case "months":
+			return `Fa ${getPositionElement(date, 0)} mesos`;
+		case "year":
+			return `Fa ${getPositionElement(date, 0)} any`;
+		case "years":
+			return `Fa ${getPositionElement(date, 0)} anys`;
+	}
+};
+
+export default getLanguageCount;
