b8ec2af2433c4ec617933f7fa14cc5ce59625ef0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-01-04 10:42:02 +0100

    Grouped for better styling responsiveness

--- client/src/components/SingleDay/SingleDay.tsx
+++ client/src/components/SingleDay/SingleDay.tsx
@@ -17,66 +17,70 @@ const SingleDay = (day: Day) => {
 	return (
 		<div className="singleday">
 			<p className="singleday__date">{date}</p>
-			<p
-				className={`singleday__icon ${updateWater === false ? "singleday__icon--greyScale" : ""}`}
-				onClick={() => {
-					setUpdateWater(!updateWater);
-				}}
-			>
-				💧
-			</p>
-			<p
-				className={`singleday__icon ${
-					updateFertilizer === false ? "singleday__icon--greyScale" : ""
-				}`}
-				onClick={() => {
-					setUpdateFertilizer(!updateFertilizer);
-				}}
-			>
-				🔋
-			</p>
-			<p
-				className={`singleday__icon ${
-					updateTransplant === false ? "singleday__icon--greyScale" : ""
-				}`}
-				onClick={() => {
-					setUpdateTransplant(!updateTransplant);
-				}}
-			>
-				🪴
-			</p>
-			<button
-				className="singleday__button"
-				disabled={
-					water === updateWater &&
-					fertilizer === updateFertilizer &&
-					transplant === updateTransplant &&
-					comment === updateComment
-						? true
-						: false
-				}
-				onClick={() => {
-					handleUpdate({
-						space,
-						id,
-						date,
-						water: updateWater,
-						fertilizer: updateFertilizer,
-						transplant: updateTransplant,
-						comment: updateComment,
-					});
-				}}
-			>
-				Actualitza
-			</button>
-			<button
-				className="singleday__button"
-				onClick={() => {
-					handleDelete([space, id]);
-				}}
-			>
-				🗑️
-			</button>
+			<div className="singleday__group">
+				<p
+					className={`singleday__icon ${updateWater === false ? "singleday__icon--greyScale" : ""}`}
+					onClick={() => {
+						setUpdateWater(!updateWater);
+					}}
+				>
+					💧
+				</p>
+				<p
+					className={`singleday__icon ${
+						updateFertilizer === false ? "singleday__icon--greyScale" : ""
+					}`}
+					onClick={() => {
+						setUpdateFertilizer(!updateFertilizer);
+					}}
+				>
+					🔋
+				</p>
+				<p
+					className={`singleday__icon ${
+						updateTransplant === false ? "singleday__icon--greyScale" : ""
+					}`}
+					onClick={() => {
+						setUpdateTransplant(!updateTransplant);
+					}}
+				>
+					🪴
+				</p>
+			</div>
+			<div className="singleday__group">
+				<button
+					className="singleday__button"
+					disabled={
+						water === updateWater &&
+						fertilizer === updateFertilizer &&
+						transplant === updateTransplant &&
+						comment === updateComment
+							? true
+							: false
+					}
+					onClick={() => {
+						handleUpdate({
+							space,
+							id,
+							date,
+							water: updateWater,
+							fertilizer: updateFertilizer,
+							transplant: updateTransplant,
+							comment: updateComment,
+						});
+					}}
+				>
+					Actualitza
+				</button>
+				<button
+					className="singleday__button"
+					onClick={() => {
+						handleDelete([space, id]);
+					}}
+				>
+					🗑️
+				</button>
+			</div>
 			<textarea
 				className="singleday__textarea"
 				defaultValue={comment}
