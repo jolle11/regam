526914e6b372bc457e16ad32647f810e2bb0d6a3
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-01-04 10:38:42 +0100

    Used vite-fonts plugin and set fonts for the page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePluginFonts({
+			google: {
+				families: [
+					"Pacifico", // font-family: 'Pacifico', cursive;
+					{
+						name: "Josefin Sans",
+						styles: "wght@200;400;700",
+						defer: true,
+						// font-family: 'Josefin Sans', sans-serif;
+					},
+				],
+			},
+		}),
+	],
 });
